0468783f82e75ecd3040ded83423440ef8468824
Kurniawanz11 <kurniawanzgithub@gmail.com>
2026-05-28 06:44:06 +0700

    Tambah machine learning pipeline

---
+++ Membangun_model/modelling.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+import joblib
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import os
+
+
+mlflow.set_tracking_uri("file:../mlruns")
+
+# Load data
+X_train = pd.read_csv("../preprocessing/X_train.csv")
+X_test = pd.read_csv("../preprocessing/X_test.csv")
+
+y_train = pd.read_csv("../preprocessing/y_train.csv")
+y_test = pd.read_csv("../preprocessing/y_test.csv")
+
+
+# MLflow experiment
+mlflow.set_experiment("Heart_Disease_Experiment")
+
+
+with mlflow.start_run():
+
+    # Model
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    # Training
+    model.fit(X_train, y_train.values.ravel())
+
+    # Predict
+    y_pred = model.predict(X_test)
+
+    # Accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Logging parameter
+    mlflow.log_param("n_estimators", 100)
+
+    # Logging metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Save model
+    joblib.dump(model, "random_forest_model.pkl")
+
+    # Logging artifact
+    mlflow.log_artifact("random_forest_model.pkl")
+
+    # Classification report
+    report = classification_report(y_test, y_pred)
+
+    with open("classification_report.txt", "w") as f:
+        f.write(report)
+
+    mlflow.log_artifact("classification_report.txt")
+
+    # Confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    with open("confusion_matrix.txt", "w") as f:
+        f.write(str(cm))
+
+    mlflow.log_artifact("confusion_matrix.txt")
+
+    # Log model
+    mlflow.sklearn.log_model(
+        model,
+        "random_forest_model"
+    )
+
+    print("Training selesai")
+    print("Accuracy:", accuracy)
---
+++ Membangun_model/modelling_tuning.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+import dagshub
+import joblib
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+import dagshub
+
+dagshub.init(
+    repo_owner="Kurniawanz11",
+    repo_name="heart-disease-mlops",
+    mlflow=True
+)
+
+
+# Tracking URI
+# mlflow.set_tracking_uri("file:../mlruns")
+
+# Experiment
+mlflow.set_experiment("Heart_Disease_Tuning")
+
+
+# Load data
+X_train = pd.read_csv("../preprocessing/X_train.csv")
+X_test = pd.read_csv("../preprocessing/X_test.csv")
+
+y_train = pd.read_csv("../preprocessing/y_train.csv")
+y_test = pd.read_csv("../preprocessing/y_test.csv")
+
+
+with mlflow.start_run():
+
+    # Parameter tuning
+    param_grid = {
+
+        "n_estimators": [50, 100],
+
+        "max_depth": [5, 10],
+
+        "min_samples_split": [2, 5]
+    }
+
+    # Model
+    rf = RandomForestClassifier(
+        random_state=42
+    )
+
+    # Grid Search
+    grid_search = GridSearchCV(
+
+        estimator=rf,
+
+        param_grid=param_grid,
+
+        cv=3,
+
+        scoring="accuracy",
+
+        n_jobs=-1
+    )
+
+    # Training
+    grid_search.fit(
+        X_train,
+        y_train.values.ravel()
+    )
+
+    # Best model
+    best_model = grid_search.best_estimator_
+
+    # Prediction
+    y_pred = best_model.predict(X_test)
+
+    # Accuracy
+    accuracy = accuracy_score(
+        y_test,
+        y_pred
+    )
+
+    # Best params
+    best_params = grid_search.best_params_
+
+    print("Best Parameter:")
+    print(best_params)
+
+    print("Accuracy:")
+    print(accuracy)
+
+    # Logging parameter
+    mlflow.log_params(best_params)
+
+    # Logging metric
+    mlflow.log_metric(
+        "accuracy",
+        accuracy
+    )
+
+    # Save model
+    joblib.dump(
+        best_model,
+        "best_random_forest.pkl"
+    )
+
+    # Log artifact
+    mlflow.log_artifact(
+        "best_random_forest.pkl"
+    )
+
+    # Classification report
+    report = classification_report(
+        y_test,
+        y_pred
+    )
+
+    with open(
+        "classification_report_tuning.txt",
+        "w"
+    ) as f:
+
+        f.write(report)
+
+    mlflow.log_artifact(
+        "classification_report_tuning.txt"
+    )
+
+    # Log model
+    mlflow.sklearn.log_model(
+
+        sk_model=best_model,
+
+        artifact_path="best_model",
+
+        input_example=X_train.iloc[:5]
+    )
+
+    print("Hyperparameter tuning selesai")
